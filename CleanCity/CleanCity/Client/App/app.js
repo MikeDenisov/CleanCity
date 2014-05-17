4150a1225a4bcec65aab0395ce3b748739e38a88
MikeDenisov <denisov.ua@gmail.com>
2014-05-18 02:51:44 +0300

    Modify routing

--- CleanCity/CleanCity/Client/App/app.js
+++ CleanCity/CleanCity/Client/App/app.js
@@ -1,6 +1,7 @@
 ﻿'use strict';
 
-var app = angular.module('noGarbage', [
+var app = angular
+    .module('noGarbage', [
     // Angular modules 
     'ngAnimate',        // animations
     'ngRoute',          // routing
@@ -17,37 +18,25 @@ var app = angular.module('noGarbage', [
 
         // 3rd Party Modules
        // 'ui.bootstrap'      // ui-bootstrap (ex: carousel, pagination, dialog)
-]);
+    ]);
 
-// Handle routing errors and success events
-app.run(['$route', function ($route) {
-    // Include $route to kick start the router.
-}]);
+app.constant("routes", {
+    user: "/user",
+    events: "/events",
+    notFound: "/notFound",
+    error: "/error",
+    main: "/"
+});
 
-app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-    $httpProvider.defaults.headers.common = { "AccessToken": "aaaaaaaaaaaaaaaa Token" };
+app.config(['$routeProvider', '$httpProvider', 'routes', function ($routeProvider, $httpProvider, routes) {
+    //$httpProvider.defaults.headers.common = { "AccessToken": "aaaaaaaaaaaaaaaa Token" };
+    var pathToIncs = 'Client/view/';
 
-    $routeProvider.
-    when('/user', {
-        templateUrl: 'Client/view/userInfo.html',
-        controller: 'UserCtrl'
-    }).
-
-        when('/events', {
-            templateUrl: 'Client/view/events.html',
-            controller: 'EventsCtrl'
-        }).
-
-    when('/notFound', {
-        templateUrl: 'Client/view/notFound.html'
-    }).
-    when('/error', {
-        templateUrl: 'Client/view/error.html'
-    }).
-    when('/', {
-        templateUrl: 'Client/view/main.html'
-    }).
-    otherwise({
-        redirectTo: '/notFound'
-    });
+    $routeProvider
+        .when(routes.user, { templateUrl: pathToIncs + 'userInfo.html', controller: 'UserCtrl' })
+        .when(routes.events, { templateUrl: pathToIncs + 'events.html', controller: 'EventsCtrl' })
+        .when(routes.notFound, { templateUrl: pathToIncs + 'notFound.html' })
+        .when(routes.error, { templateUrl: pathToIncs + 'error.html' })
+        .when(routes.main, { templateUrl: pathToIncs + 'main.html' })
+        .otherwise({ redirectTo: routes.notFound });
 }]);
