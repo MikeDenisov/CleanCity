d5a0c82f92611fdb70f63ac867a1edbaf4c9fe5d
torasko <nygel@super-air.local>
2014-05-18 16:56:52 +0300

    events

--- CleanCity/CleanCity/Client/App/controllers/eventsController.js
+++ CleanCity/CleanCity/Client/App/controllers/eventsController.js
@@ -1,8 +1,8 @@
-angular.module('noGarbageControllers', [])
-    .controller('MainCtrl', [
+noGarbageControllers.controller('eventsCtrl', [
         '$scope', 'Garbage',
         function ($scope, Garbage) {
-
+            $scope.garbageMarkers = Garbage.getGarbageLocationList();
+alert ('fsd');
             $scope.smallSidebar = true;
             $scope.showAbout = false;
 
